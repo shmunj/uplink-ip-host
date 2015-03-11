3dafd3fbe2ee77ddb61318508e27f1b05d6bc664
shkrulj <shkrulj@churchbank.(none)>
2015-03-11 12:10:40 +0100

    First commit

---
+++ README.txt
@@ -0,0 +1,7 @@
+This small tool is an attempt to replicate one of the (simplest) tools from Uplink, a great video game about hacking.
+
+The tool pretty much just converts a given IP address to hostname and vice versa, but it looks like the tool from the game. Maybe I'll do a similar GUI using other libraries for other tools like John the Ripper etc in order to make a whole Uplink themed toolbox for script kiddies.
+
+It is made in Pygame because Uplink is a game. At the time I thought it was funny...
+
+(The script uses great little module called EzText: http://www.pygame.org/project-EzText-920-.html)
---
+++ ip-host.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+import os,socket,pygame,sys,eztext
+from pygame import *
+#os.chdir('/home/shmunj/bin/ip-host/')
+
+def check_address(address):
+    try:
+        address.split('.') == True
+        for n in address.split('.'):
+            try:
+                int(n)
+            except:
+                x = socket.gethostbyname(address)
+                return x
+        x = socket.gethostbyaddr(address)[0]
+    except:
+        x = socket.gethostbyname(address)
+    return x
+
+#fixed window position
+wx,wy = 0,0
+os.environ['SDL_VIDEO_WINDOW_POS'] = "%d,%d" % (wx,wy)
+
+pygame.init()
+
+w,h = 320,16
+screen = pygame.display.set_mode((w,h), pygame.NOFRAME)
+
+#eztext
+txtbx = eztext.Input(maxlength=60, color=(255,255,255), prompt='HOST: ')
+font = pygame.font.Font('airstrip.ttf',10)
+txtbx.set_pos(2,2)
+txtbx.set_font(font)
+
+clock = pygame.time.Clock()
+
+while 1:
+    mx,my = pygame.mouse.get_pos()
+
+    events = pygame.event.get()
+    for event in events:
+        if event.type == KEYDOWN and event.key == K_ESCAPE:
+            sys.exit()
+        if event.type == KEYDOWN and event.key == K_RETURN:
+            address = txtbx.update(events)
+            try:
+                result = check_address(address)
+            except:
+                result = 'ERROR'
+            txtbx.value = result
+
+    screen.fill((0,0,100))
+    
+    txtbx.update(events)
+    txtbx.draw(screen)
+    pygame.display.update()
